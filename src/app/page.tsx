73057c827aa6a7cbc453e3788eb42062059aaacd
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-07 09:59:12 +0530

    Modifications in the index Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,7 @@
 "use client";
-import { urlEndpoint } from "@/providers/providers";
-import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
-import { useState } from "react";
 
-const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-const authenticator = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/api/auth");
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Request failed with status ${response.status}: ${errorText}`
-      );
-    }
-
-    const data = await response.json();
-    const { signature, expire, token } = data;
-    return { signature, expire, token };
-  } catch (error) {
-    throw new Error(`Authentication request failed: ${error.message}`);
-  }
-};
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const [filePath, setFilePath] = useState("");
-  return (
-    <ImageKitProvider
-      publicKey={publicKey}
-      urlEndpoint={urlEndpoint}
-      authenticator={authenticator}
-    >
-      {filePath && (
-        <IKImage path={filePath} width={300} height={500} alt="Alt text" />
-      )}
-      <div>
-        <h2>File upload</h2>
-        <IKUpload
-          fileName="test-upload.png"
-          onError={(error) => {
-            console.log(error);
-          }}
-          onSuccess={(response) => {
-            console.log(response);
-            setFilePath(response.filePath);
-          }}
-        />
-      </div>
-    </ImageKitProvider>
-  );
+  redirect("/search?q=");
 }
