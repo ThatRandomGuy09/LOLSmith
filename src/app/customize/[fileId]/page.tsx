e8a6ab634e8aa4669212f0dc880cb2fcebd2e151
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-07 11:07:50 +0530

    Added the Customize Panel in the Page

--- src/app/customize/[fileId]/page.tsx
+++ src/app/customize/[fileId]/page.tsx
@@ -1,3 +1,4 @@
+import { CustomizePanel } from "@/components/customize-panel";
 import { imagekit } from "@/lib/image-kit";
 import { unstable_noStore } from "next/cache";
 
@@ -9,6 +10,16 @@ export default async function CustomizePage({
   unstable_noStore();
 
   const file = await imagekit.getFileDetails(params.fileId);
-  console.log(file);
-  return <div className="container mx-auto space-y-8 py-8 px-4"></div>;
+
+  return (
+    <div className="container mx-auto space-y-8 py-8 px-4">
+      <CustomizePanel
+        file={{
+          filePath: file.filePath,
+          name: file.name,
+          fileId: file.fileId,
+        }}
+      />
+    </div>
+  );
 }
