a9b37b666af3d2974ed97f358c13d1d841050f63
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-08 14:52:05 +0530

    Customized panel refactored

--- src/components/customize-panel.tsx
+++ src/components/customize-panel.tsx
@@ -6,35 +6,22 @@ import { IKImage } from "imagekitio-next";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { useState } from "react";
+import { Slider } from "./ui/slider";
+import { TextOverlay } from "./text-overlay";
 export function CustomizePanel({
   file,
 }: {
   file: Pick<FileObject, "filePath" | "name" | "fileId">;
 }) {
-  const [textOverlay1, setTextOverlay1] = useState("");
-  const transformations = [];
-  if (textOverlay1) {
-    transformations.push({ raw: `l-text,i-${textOverlay1},fs-50 l-end` });
-    console.log(transformations);
-  }
+  const [textTransformation, setTextTransformations] = useState<
+    Record<string, { raw: string }>
+  >({});
   return (
     <div className="grid grid-cols-2 gap-8">
-      <form>
-        <Label>Text Overlay</Label>
-        <Input
-          type="textOverlay1"
-          onChange={(e) => setTextOverlay1(e.target.value)}
-          value={textOverlay1}
-        />
+      <form className="space-y-4">
+        <TextOverlay file={file} />
       </form>
-      <IKImage
-        path={file.filePath}
-        alt={file.name}
-        urlEndpoint={urlEndpoint}
-        width={300}
-        height={300}
-        transformation={transformations}
-      />
+      <IKImage path={file.filePath} alt={file.name} urlEndpoint={urlEndpoint} />
     </div>
   );
 }
