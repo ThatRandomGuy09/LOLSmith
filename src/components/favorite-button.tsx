accc7ad140e72ff5ad57c2c9402fa1092bfc8033
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-09 10:06:11 +0530

    Favourite Button Done

---
+++ src/components/favorite-button.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Heart } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { HeartFilledIcon } from "@radix-ui/react-icons";
+
+export function FavoriteButton({
+  isFavorited,
+  fileId,
+  filePath,
+  pathToRevalidate,
+}: {
+  isFavorited: boolean;
+  fileId: string;
+  filePath: string;
+  pathToRevalidate: string;
+}) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <form>
+            <Button type="submit" variant="outline">
+              {isFavorited ? <HeartFilledIcon /> : <Heart />}
+            </Button>
+          </form>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{isFavorited ? "Unfavorite Meme" : "Favorite Meme"}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
