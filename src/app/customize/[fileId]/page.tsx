29ecc5d6ec38ec71001a56c844166c02070bde36
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-12 19:55:05 +0530

    Added Favourite feature in Customize Page

--- src/app/customize/[fileId]/page.tsx
+++ src/app/customize/[fileId]/page.tsx
@@ -1,6 +1,8 @@
 import { CustomizePanel } from "@/components/customize-panel";
 import { imagekit } from "@/lib/image-kit";
 import { unstable_noStore } from "next/cache";
+import { getFavoriteMeme } from "./loader";
+import { auth } from "@/auth";
 
 export default async function CustomizePage({
   params,
@@ -9,16 +11,21 @@ export default async function CustomizePage({
 }) {
   unstable_noStore();
 
+  const session = await auth();
+
   const file = await imagekit.getFileDetails(params.fileId);
+  const isFavorited = session ? await getFavoriteMeme(params.fileId) : false;
 
   return (
     <div className="container mx-auto space-y-8 py-8 px-4">
       <CustomizePanel
+        isAuthenticated={!!session}
         file={{
           filePath: file.filePath,
           name: file.name,
           fileId: file.fileId,
         }}
+        isFavorited={isFavorited}
       />
     </div>
   );
