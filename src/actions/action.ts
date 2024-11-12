0cbc95fc81e7988341599e9785c11b5dc6f6d5ec
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-12 19:54:26 +0530

    Server actions for Favourite meme

---
+++ src/actions/action.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { db } from "@/app/db/db";
+import { favorites } from "@/app/db/schema";
+import { assertAuthenticated } from "../lib/auth-utils";
+import { eq, and, sql } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+
+export async function toggleFavoriteMemeAction(
+  fileId: string,
+  filePath: string,
+  pathToRevalidate: string
+) {
+  const userId = await assertAuthenticated();
+
+  const favorite = await db.query.favorites.findFirst({
+    where: and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)),
+  });
+
+  if (favorite) {
+    await db
+      .delete(favorites)
+      .where(and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)));
+  } else {
+    await db.insert(favorites).values({
+      userId,
+      memeId: fileId,
+    });
+  }
+
+  revalidatePath(pathToRevalidate);
+}
