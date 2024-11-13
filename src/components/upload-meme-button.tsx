d0a04a0c0ecc0d461f3191a1700b15298098e907
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-13 12:58:15 +0530

    Code Cleaning

--- src/components/upload-meme-button.tsx
+++ src/components/upload-meme-button.tsx
@@ -19,7 +19,6 @@ import { useRouter } from "next/navigation";
 import { Spinner } from "./spinner";
 
 export function UploadMemeButton() {
-  
   const uploadInputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
   const [displayName, setDisplayName] = useState("");
