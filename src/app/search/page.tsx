c19f0d3519eafcdc1364e57f5ce2b59796e9ed9e
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-13 13:00:14 +0530

    Utility functions added in Search Page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,8 @@
-import { UploadMemeButton } from "@/components/upload-meme-button";
-import ImageKit from "imagekit";
-import { ResultsList } from "@/components/results-list";
 import { unstable_noStore } from "next/cache";
-import { imagekit } from "@/lib/image-kit";
+import { ResultsList } from "../../components/results-list";
+import { UploadMemeButton } from "../../components/upload-meme-button";
+import { imagekit } from "../../lib/image-kit";
+import { getFavoriteCounts } from "./loaders";
 
 export default async function SearchPage({
   searchParams,
@@ -14,13 +14,19 @@ export default async function SearchPage({
   const files = await imagekit.listFiles({
     tags: searchParams.q,
   });
+
+  const favoriteCounts = await getFavoriteCounts(
+    files.map((file) => file.fileId)
+  );
+
   return (
     <div className="container mx-auto space-y-8 py-8 px-4">
       <div className="flex items-center justify-between">
         <h1 className="text-4xl font-bold">Search Results</h1>
         <UploadMemeButton />
       </div>
-      <ResultsList files={files} counts={[]} />
+
+      <ResultsList files={files} counts={favoriteCounts} />
     </div>
   );
 }
