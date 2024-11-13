87c20950bf17a3038ecee6f864e8f756d8bd8045
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-13 12:56:41 +0530

    Updated Server Action with Favourite Count added

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,7 +1,6 @@
 "use server";
-
 import { db } from "@/app/db/db";
-import { favorites } from "@/app/db/schema";
+import { favoriteCounts, favorites } from "@/app/db/schema";
 import { assertAuthenticated } from "../lib/auth-utils";
 import { eq, and, sql } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
@@ -21,11 +20,30 @@ export async function toggleFavoriteMemeAction(
     await db
       .delete(favorites)
       .where(and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)));
+    await db
+      .update(favoriteCounts)
+      .set({
+        count: sql`${favoriteCounts.count} - 1`,
+      })
+      .where(eq(favoriteCounts.memeId, fileId));
   } else {
     await db.insert(favorites).values({
       userId,
       memeId: fileId,
+      filePath: filePath,
     });
+    await db
+      .insert(favoriteCounts)
+      .values({
+        memeId: fileId,
+        count: 1,
+      })
+      .onConflictDoUpdate({
+        set: {
+          count: sql`${favoriteCounts.count} + 1`,
+        },
+        target: favoriteCounts.memeId,
+      });
   }
 
   revalidatePath(pathToRevalidate);
